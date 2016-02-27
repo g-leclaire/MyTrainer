eeb629236be07249cec934cbccc83ffcacded726
g-leclaire <geoffrey.leclaire@polymtl.ca>
2016-02-26 19:44:40 -0500

    Tried integrating drawer to main activity, drawer is black.

--- app/src/main/java/com/geoff/mytrainer/MainActivity.java
+++ app/src/main/java/com/geoff/mytrainer/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -37,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private String[] sets;
     private String[] rests;
 
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     public final static String EXTRA_MESSAGE = "com.geoff.myTrainer.MESSAGE";
 
     // TODO: Delete.
@@ -64,6 +69,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         .setAction("Action", null).show();
             }
         });
+
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         SharedPreferences sharedPref = getSharedPreferences("Exercises", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
@@ -237,6 +252,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             View newView = list.getChildAt(position + 1);
             // Show the item options.
             showItemOptions(newView);
+        }
+    }
+
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
         }
     }
 }
