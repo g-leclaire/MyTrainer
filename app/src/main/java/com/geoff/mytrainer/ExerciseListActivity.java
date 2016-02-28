ef94d55d3d4ddd52b9191f90a3c8f4298f6fcb59
g-leclaire <geoffrey.leclaire@polymtl.ca>
2016-02-28 12:20:49 -0500

    Drawer layout customized. Missing drawables,

--- app/src/main/java/com/geoff/mytrainer/ExerciseListActivity.java
+++ app/src/main/java/com/geoff/mytrainer/ExerciseListActivity.java
@@ -158,17 +158,15 @@ public class ExerciseListActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_workout1) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_workout2) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_workoutlog) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_statistics) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
